4ddd2878f2c6f6dfa518ef1379b206bb975ac6be
najlaajou <najlaajou23@gmail.com>
2020-03-15 18:11:36 +0100

    Added new feature: modify/update the name of the existing elements

--- JsReview/js/script.js
+++ JsReview/js/script.js
@@ -1,10 +1,20 @@
 $('document').ready(function(){
+var nb = 0;
 
 function addElement(str1,str2){
-var e = document.createElement("div");
-var cont = "<b>Nom:</b>"+str1+"<br><b>Age:</b>"+str2+"<br><hr align='center'>";
+    nb++;
+    var e = document.createElement("div");
+    var cont = "<b>Nom:</b><span id='nom"+nb+"'>"+str1+"</span><br><b>Age:</b>"+str2+"<br><center><button id='bt"+nb+"'>Modifier</button></center><hr align='center'>";
 e.innerHTML = cont;
 $("#elems").append(e);
+$('#bt'+nb).click(function(){
+    var currentId = this.id;
+    var currentIndex = currentId.substring(2,currentId.length);
+    var newName = prompt("Donnez le nom à modifier:");
+
+    var idd = "nom"+currentIndex;
+    $("#"+idd).html(newName);
+})
 }
 
 
